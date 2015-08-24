2cf0c1acac349540789de36b0c47bdb61cb7988f
Antriksh <antrix.sharma@gmail.com>
2015-08-24 18:01:04 -0400

    mod4 and mod5

--- test.c
+++ test.c
@@ -3,10 +3,14 @@
 void main() 
 {
 	printf("Hello world");
+	
 	printf("mod 1");
 	printf("mod 2");
 	printf("mod 2-2");
 	printf("mod 3");
 	printf("mod 3-3");
 	printf("mod 3-3-3");
+
+	printf("mod 4");
+	printf("mod 5");
 }
